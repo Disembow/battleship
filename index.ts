382d67899a0ce1ac9be6970f437b771d57a7376e
Disembow <e_naletov@mail.ru>
2023-07-07 17:50:55 +0200

    feat: add ports to process.env

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import { httpServer } from './src/http_server/index.js';
+import { config } from 'dotenv';
+import { http_server } from './src/http_server/index.js';
 import { ws_server } from './src/ws_server/server.js';
 
-const HTTP_PORT = 8181;
-const WEBSOCKET_PORT = 3000;
+config();
 
-httpServer.listen(HTTP_PORT, () => {
-  console.log(`Start static http server on the ${HTTP_PORT} port!`);
-});
+const HTTP_PORT = Number(process.env.HTTP_PORT) ?? 8181;
+const WEBSOCKET_PORT = Number(process.env.WEBSOCKET_PORT) ?? 3000;
 
+http_server(HTTP_PORT);
 ws_server(WEBSOCKET_PORT);
--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,11 +2,11 @@ import fs from 'node:fs';
 import path from 'node:path';
 import http from 'node:http';
 
-export const httpServer = http.createServer(function (req, res) {
+const httpServer = http.createServer(function (req, res) {
   const __dirname = path.resolve(path.dirname(''));
   const file_path = path.join(
     __dirname,
-    req.url === '/' ? '/front/index.html' : '/front' + req.url
+    req.url === '/' ? '/front/index.html' : '/front' + req.url,
   );
 
   fs.readFile(file_path, function (err, data) {
@@ -20,3 +20,8 @@ export const httpServer = http.createServer(function (req, res) {
     res.end(data);
   });
 });
+
+export const http_server = (port: number) =>
+  httpServer.listen(port, () => {
+    console.log(`Start static http server on the ${port} port!`);
+  });
