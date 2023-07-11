19e76548c1e8be53dde1a124c53562a3a567c34b
Disembow <e_naletov@mail.ru>
2023-07-11 10:24:08 +0200

    refactor: iskilled func

--- src/ws_server/constants.ts
+++ src/ws_server/constants.ts
@@ -1,1 +1,2 @@
 export const USERS_PER_GAME = 2;
+export const FIELD_SIDE_SIZE = 10;
--- src/ws_server/utils/isKilled.ts
+++ src/ws_server/utils/isKilled.ts
@@ -1,21 +1,10 @@
 import { TStringCoords } from './setInitialShipState.js';
 
 export const isKilled = (init: TStringCoords, final: TStringCoords) => {
-  let result: [number, string[]] | null = null;
-
   init.forEach((e, i) => {
     if (JSON.stringify(e) === JSON.stringify(final[i])) {
-      result = [i, e];
+      init.splice(i, 1);
+      final.splice(i, 1);
     }
   });
-
-  // remove ships after it was killed
-  if (result !== null) {
-    const [index, _] = result as [number, string[]];
-
-    init.splice(index, 1);
-    final.splice(index, 1);
-  }
-
-  return result;
 };
