67f54ef049aa1b70396f05242fb0064c2ab3ce9f
Disembow <e_naletov@mail.ru>
2023-07-14 10:17:13 +0200

    refactor: remove code duplicating

--- src/ws_server/utils/randomShotCoords.ts
+++ src/ws_server/utils/randomShotCoords.ts
@@ -1,6 +1,8 @@
 export const randomShotCoords = (min: number, max: number) => {
-  const x = Math.sqrt(Math.pow(Math.round(min - 0.5 + Math.random() * (max - min)), 2));
-  const y = Math.sqrt(Math.pow(Math.round(min - 0.5 + Math.random() * (max - min)), 2));
+  const random = () => Math.sqrt(Math.pow(Math.round(min - 0.5 + Math.random() * (max - min)), 2));
+
+  const x = random();
+  const y = random();
 
   return [x, y];
 };
